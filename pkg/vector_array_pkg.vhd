fdea5364ee3a568df9b463476334c37cabba0f52
pingulorenzo <lorenzodagostino@gmail.com>
2019-05-16 23:20:57 +0200

    Second Push
    C'era un errore nell'aritmetica, facendo i calcoli non tornava, adesso funziona. C'era poi, nel package creato per gestire gli array di logic_vectors, l'array degli input con un input di troppo (L'input 0, quello del Bias, è implicitamente fisso ad 1, quindi non serve)

--- pkg/vector_array_pkg.vhd
+++ pkg/vector_array_pkg.vhd
@@ -4,5 +4,5 @@ use ieee.numeric_std.all;
 
 package vector_array_pkg is
         type weight_array is array(10 downto 0) of std_logic_vector(8 downto 0);
-		type input_array is array(10 downto 0) of std_logic_vector(7 downto 0);
+		type input_array is array(10 downto 1) of std_logic_vector(7 downto 0);
 end package;
--- src/Aritmethic.vhd
+++ src/Aritmethic.vhd
@@ -26,27 +26,20 @@ architecture internal of Arithmetic is
 	signal bias : std_logic_vector (BitW-1 downto 0);
 	
 	-- signals for the weighted inputs
-	signal xw1, xw2, xw3, xw4, xw5, xw6, xw7, xw8, xw9, xw10 : signed ((BitX + BitW)-1 downto 0); -- 17 Bits
+	signal xw1, xw2, xw3, xw4, xw5, xw6, xw7, xw8, xw9, xw10 : std_logic_vector ((BitX + BitW)-1 downto 0); -- 17 Bits
 	
 	-- xAxB is the signal for thew result of A + B
-	signal x1x2, x3x4, x5x6, x7x8, x9x10 : std_logic_vector ((BitX + BitW)-1 downto 0); -- 17 Bits
+	signal x1x2, x3x4, x5x6, x7x8, x9x10  : std_logic_vector ((BitX + BitW) downto 0); -- 18 Bits
 	
-	-- concatenation of previous inputs with addition overflow
-	signal x1x2_ow, x3x4_ow, x5x6_ow, x7x8_ow, x9x10_ow : std_logic_vector ((BitX + BitW) downto 0); -- 18 Bits
-	
-	-- x_low is the sum of 1, 2, 3 and 4	
+	-- x_low is the sum of 1, 2, 3 and 4
 	-- x_mid is the sum of 5, 6 and bias
 	-- x_high is the sum of 7, 8, 9, 10
 	-- ext_bias is the bit extension of bias
-	signal x_low, x_mid, x_high, ext_bias : std_logic_vector ((BitX + BitW) downto 0); -- 18 Bits
-	
-	-- x_lowhigh is the sum of x_low and x_high
-	-- concatenation of previous inputs with addition overflow
-	signal x_low_ow, x_high_ow, x_lowhigh : std_logic_vector ((BitX + BitW)+1 downto 0); -- 19 Bits
-	
+	signal x_low, x_mid, x_high, ext_bias : std_logic_vector ((BitX + BitW)+1 downto 0); -- 19 Bits
+		
 	-- ext_x_mid is the extension of x_mid
 	-- x_end is the sum of x_lowhigh and ext_x_mid
-	signal x_lowhigh_ow, ext_x_mid, x_end : std_logic_vector ((BitX + BitW)+2 downto 0); -- 20 Bits
+	signal x_lowhigh, ext_x_mid, x_end : std_logic_vector ((BitX + BitW)+2 downto 0); -- 20 Bits
 	
 	-- x_end_trunc the 8 bit truncated version
 	signal x_end_trunc : std_logic_vector (BitO-1 downto 0); -- 8 Bits
@@ -68,52 +61,40 @@ begin
 	bias <= w(0);
 	
 	-- Producing weighted inputs	
-	xw1 <= (signed(x(1)) * signed(w(1)));
-	xw2 <= (signed(x(2)) * signed(w(2)));
-	xw3 <= (signed(x(3)) * signed(w(3)));
-	xw4 <= (signed(x(4)) * signed(w(4)));
-	xw5 <= (signed(x(5)) * signed(w(5)));
-	xw6 <= (signed(x(6)) * signed(w(6)));
-	xw7 <= (signed(x(7)) * signed(w(7)));
-	xw8 <= (signed(x(8)) * signed(w(8)));
-	xw9 <= (signed(x(9)) * signed(w(9)));
-	xw10 <= (signed(x(10)) * signed(w(10)));	
+	xw1 <= std_logic_vector(signed(x(1)) * signed(w(1)));
+	xw2 <= std_logic_vector(signed(x(2)) * signed(w(2)));
+	xw3 <= std_logic_vector(signed(x(3)) * signed(w(3)));
+	xw4 <= std_logic_vector(signed(x(4)) * signed(w(4)));
+	xw5 <= std_logic_vector(signed(x(5)) * signed(w(5)));
+	xw6 <= std_logic_vector(signed(x(6)) * signed(w(6)));
+	xw7 <= std_logic_vector(signed(x(7)) * signed(w(7)));
+	xw8 <= std_logic_vector(signed(x(8)) * signed(w(8)));
+	xw9 <= std_logic_vector(signed(x(9)) * signed(w(9)));
+	xw10 <= std_logic_vector(signed(x(10)) * signed(w(10)));	
 	
 	-- First 5 adders
-	x1x2 <= std_logic_vector(xw1 + xw2);
-	x3x4 <= std_logic_vector(xw3 + xw4);
-	x5x6 <= std_logic_vector(xw5 + xw6);
-	x7x8 <= std_logic_vector(xw7 + xw8);
-	x9x10 <= std_logic_vector(xw9 + xw10);
-	
-	-- Managing first 5 adders overflows and concatenating	
-	x1x2_ow <= ((xw1(16) AND xw2(16)) OR (not(x1x2(16)) AND xw1(16)) OR (not(x1x2(16)) AND xw2(16))) & x1x2;
-	x3x4_ow <= ((xw3(16) AND xw4(16)) OR (not(x3x4(16)) AND xw3(16)) OR (not(x3x4(16)) AND xw4(16))) & x3x4;
-	x5x6_ow <= ((xw5(16) AND xw6(16)) OR (not(x5x6(16)) AND xw5(16)) OR (not(x5x6(16)) AND xw6(16))) & x5x6;
-	x7x8_ow <= ((xw7(16) AND xw8(16)) OR (not(x7x8(16)) AND xw7(16)) OR (not(x7x8(16)) AND xw8(16))) & x7x8;
-	x9x10_ow <= ((xw9(16) AND xw10(16)) OR (not(x9x10(16)) AND xw9(16)) OR (not(x9x10(16)) AND xw10(16))) & x9x10;	
+	x1x2 <= std_logic_vector(signed(xw1(16) & xw1) + signed(xw2(16) & xw2));
+	x3x4 <= std_logic_vector(signed(xw3(16) & xw3) + signed(xw4(16) & xw4));
+	x5x6 <= std_logic_vector(signed(xw5(16) & xw5) + signed(xw6(16) & xw6));
+	x7x8 <= std_logic_vector(signed(xw7(16) & xw7) + signed(xw8(16) & xw8));
+	x9x10 <= std_logic_vector(signed(xw9(16) & xw9) + signed(xw10(16) & xw10));
 	
 	-- Extending bias befor addition
-	ext_bias <=  bias(8) & bias(8) & bias(8) & bias(8) & bias(8) & bias(8) & bias(8) & bias(8) & bias(8) & bias;
+	ext_bias <=  bias(8) & bias(8) & bias(8) & bias(8) & bias(8) & bias(8) & bias(8) & bias(8) & bias(8) & bias(8) & bias;
 	
 	-- Second 3 adders
-	x_low <= std_logic_vector(signed(x1x2_ow) + signed(x3x4_ow));
-	x_high <= std_logic_vector(signed(x7x8_ow) + signed(x9x10_ow));
-	x_mid <= std_logic_vector(signed(x5x6_ow) + signed(ext_bias));
-	
-	-- Concatenating with overflow bit
-	x_low_ow <= ((x1x2_ow(17) AND (x3x4_ow(17))) OR (not(x_low(17)) AND (x3x4_ow(17))) OR (x1x2_ow(17) AND (not(x_low(17))))) & x_low;
-	x_high_ow <= ((x7x8_ow(17) AND (x9x10_ow(17))) OR (not(x_high(17)) AND (x7x8_ow(17))) OR (x9x10_ow(17) AND (not(x_high(17))))) & x_high;
+	x_low <= std_logic_vector(signed(x1x2(17) & x1x2) + signed(x3x4(17) & x3x4));
+	x_high <= std_logic_vector(signed(x7x8(17) & x7x8) + signed(x9x10(17) & x9x10));
+	x_mid <= std_logic_vector(signed(x5x6(17) & x5x6) + signed(ext_bias));
 	
 	-- Second-last adder
-	x_lowhigh <= std_logic_vector(signed(x_low_ow) + signed(x_high_ow));
-	x_lowhigh_ow <= ((x_low_ow(18) AND x_high_ow(18)) OR (not(x_lowhigh(18)) AND x_high_ow(18)) OR (x_low_ow(18) AND not(x_lowhigh(18)))) & x_lowhigh;
+	x_lowhigh <= std_logic_vector(signed(x_low(18) & x_low) + signed(x_high(18) & x_high));
 	
 	-- Extending x_mid to 20 bits
-	ext_x_mid <= x_mid(17) & x_mid(17) & x_mid;
+	ext_x_mid <= x_mid(18) & x_mid;
 	
 	-- Last adder
-	x_end <= std_logic_vector(signed(x_lowhigh_ow) + signed(ext_x_mid));
+	x_end <= std_logic_vector(signed(x_lowhigh) + signed(ext_x_mid));
 	
 	-- Truncation to output
 	--x_end_trunc <= x_end(((BitX + BitW)+2) downto (((BitX + BitW)+2) - (BitO-1)));
