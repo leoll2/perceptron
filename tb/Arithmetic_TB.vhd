e4b7cb7bf50aeef0862010360c2add6019ad7b77
pingulorenzo <lorenzodagostino@gmail.com>
2019-06-01 19:14:06 +0200

    fixed Arithmetic_TB and img

--- tb/Arithmetic_TB.vhd
+++ tb/Arithmetic_TB.vhd
@@ -56,35 +56,38 @@ begin
 	begin	
 		if (rising_edge(clk_tb)) then
 			case(t) is
-                -- To get represented values divide Value by 2^8 (in case of x_tb) or by 2^9 (in case of w_tb)
-				when 0 => 
+			-- To get represented values divide Value by 2^8 (in case of x_tb) or by 2^9 (in case of w_tb)
+				when 0 =>
 					x_tb <= (others => "00000000"); -- Value: 0
 					w_tb <= (others => "000000000"); -- Value: 0
-                when 1 => 
+				when 1 =>
 					x_tb <= (others => "00000001"); -- Value: 1
 					w_tb <= (others => "000000001"); -- Value: 1
-                when 2 => 
+				when 2 => 
 					x_tb <= (others => "11111111"); -- Value: -1
 					w_tb <= (others => "111111111"); -- Value: -1
-                when 3 => 
+				when 3 => 
 					x_tb <= (others => "01111111"); -- Value: 127
 					w_tb <= (others => "011111111"); -- Value: 255
-                when 4 => 
+				when 4 => 
 					x_tb <= (others => "10000000"); -- Value: -128
-					w_tb <= (others => "100000000"); -- Value: -256
-                when 5 => 
+					w_tb <= (0 => "011111111", others => "100000000"); -- Value: -256, Bias: 255
+				when 5 => 
 					x_tb <= (others => "10000000"); -- Value: -128
 					w_tb <= (others => "011111111"); -- Value: 255
-                when 6 => 
+				when 6 => 
 					x_tb <= (others => "01111111"); -- Value: 127
-					w_tb <= (others => "100000000"); -- Value: -256
-                when 7 => 
+					w_tb <= (0 => "011111111", others => "100000000"); -- Value: -256, Bias: 255
+				when 7 => 
 					x_tb <= (1 => "00010101", others => "00000000"); -- Value: 1 => 23
 					w_tb <= (1 => "000000011", others => "000000000"); -- Value: 1 => 3
-                when 8 => 
+				when 8 => 
 					x_tb <= (1 => "00010101", others => "00000000"); -- Value: 1 => 23
 					w_tb <= (0 => "000000001", 1 => "000000011", others => "000000000"); -- Value: 0 => 1, 1 => 3
 				when 9 => 
+					x_tb <= (1 => "01000000", 2 => "01000000", 3 => "01000000", others => "00000000"); -- Value: 1, 2, 3 => 64
+					w_tb <= (1 => "010000000", 2 => "010000000", 3 => "010000000", others => "000000000"); -- Value: 0 => 1, 1 => 128
+				when 10 => 
 					end_sim <= '0';
 				when others => null;			
 			end case;
